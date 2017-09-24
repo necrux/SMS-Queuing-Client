642b624477e27797057af01933f250a0cb53cf53
wes <wes.henderson@rackspace.com>
2017-09-24 11:29:50 -0500

    Added settings config.

--- sms.py
+++ sms.py
@@ -7,6 +7,15 @@ from os import path
 base_dir = path.dirname(path.realpath(__file__))
 config = ConfigParser.ConfigParser()
 config.optionxform = str
-config.read(base_dir + '/config')
+config.read(base_dir + '/.sms')
 
-# Initialize dictionaries from 'config'.
+# Grabbing 'settings' from .sms file.
+SERVER = config.get('settings', 'server')
+if config.getboolean('settings', 'SSL') is True:
+  HANDLER = 'https://'
+  ENDPOINT = '{}{}'.format(HANDLER,SERVER)
+elif config.getboolean('settings', 'SSL') is False:
+  HANDLER = 'http://'
+  ENDPOINT = '{}{}'.format(HANDLER,SERVER)
+
+# Configuring user.
