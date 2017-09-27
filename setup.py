cc8ef061760d83ac1f53a12158232dea333ed0a7
wes <whendersonii@gmail.com>
2017-09-26 22:31:30 -0500

    Restructured for readability and to better align with best practices.

--- setup.py
+++ setup.py
@@ -3,19 +3,35 @@ from os import path
 
 home = path.expanduser('~')
 
+requirements = [
+  'configparser',
+  'requests'
+]
+
+#test_requirements = [
+#    # TODO: put package test requirements here
+#]
+
 setup(
   name = 'sms',
+  author = 'necrux',
+  description = 'A Python client for the SMS queuing service.',
+  keywords = 'sms queue queuing client',
   version = '2.0.0',
-  install_requires = ['configparser','requests'],
+  license = 'MIT',
+  url = 'https://github.com/necrux/SMS-Queuing-Client', 
+  classifiers = [
+    'Development Status :: 3 - Alpha',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2.7'
+  ],
+
+  install_requires = requirements,
   packages = ['sms'],
   entry_points = { 
     'console_scripts': [
       'sms=sms.sms:main',
     ],
    },
-  data_files = [('{}/'.format(home), ['.sms'])],
-  author = 'necrux',
-  description = 'A Python client for the SMS queuing service.',
-  keywords = 'sms queue queuing client',
-  url = 'https://github.com/necrux/SMS-Queuing-Client' 
+  data_files = [('{}/'.format(home), ['data/.sms'])]
   )
